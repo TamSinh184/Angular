1f0b1cac61c1b96f778e58994376222b31cadcaa
TamSinh <nguyenle18041995@gmail.com>
2019-07-26 10:46:49 +0700

    added file tampon

---
+++ src/app/tampon/tampon/tampon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tampon',
+  templateUrl: './tampon.component.html',
+  styleUrls: ['./tampon.component.scss']
+})
+export class TamponComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
